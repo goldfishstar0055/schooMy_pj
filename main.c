7804c611e7d2bd95692ac834ad93ba3ef5a7b124
goldfishstar0055 <kmc2542@kamiyama.ac.jp>
2026-05-07 14:21:33 +0900

    Add main.c with setup and loop functions

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <SchooMyUtilities.h>
+SchooMyUtilities scmUtils = SchooMyUtilities();
+
+float num = 0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(19, INPUT);
+  pinMode(19, OUTPUT);
+  digitalWrite(19, HIGH);
+}
+
+void loop() {
+  num = 30;
+  while ((num >= 0)) {
+    unsigned long _wt_start = millis();
+    while (millis() - _wt_start < (digitalRead(19)) * 1000UL) {
+      delay(1 * 1000);
+      num -= 1;
+    }
+  }
+  digitalWrite(19, LOW);
+}
